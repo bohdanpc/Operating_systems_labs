3106f3c96b973378ae63611dcd3a77a670d8e0f9
bohdanpc <bohdanpc@gmail.com>
2017-09-29 23:27:49 +0200

    Implement scheduler interface class

---
+++ Process_scheduling/Process_scheduling/iSchedulerBase.h
@@ -0,0 +1,19 @@
+/* file iSchedulerBase.h
+ * created: 29.09.17
+ * author: Levunets
+ */
+#pragma once
+#include <string>
+#include <fstream>
+#include <queue>
+
+template <class processType>
+class SchedulerBase {
+public:
+	virtual void ParseSchedulerFile(const std::string &inFileName) = 0;
+	virtual void BuildSchedulerResFile(const std::string &outFileName) = 0;
+	virtual void RunScheduler() = 0;
+protected:
+	std::queue<processType> processesList;
+	std::queue<processType> outputProcessesList;
+};
